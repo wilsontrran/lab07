7552e08209a3328b1bd0d285c35706d4dc05a6a6
wilsontrran <wilson.tran.345@my.csun.edu>
2016-05-04 13:31:53 -0700

    Update employee.js

--- employee.js
+++ employee.js
@@ -1,6 +1,7 @@
 var employees = [{}];
 var i = 0;
 function submit() {
+
 employees.push({});
 
 employees[i].fname = document.getElementById("fname").value;
@@ -8,14 +9,24 @@ employees[i].fname = document.getElementById("fname").value;
 employees[i].lname = document.getElementById("lname").value;
   
 employees[i].department = document.getElementById("department").value;
+
+var d = new Date();
+var days = ["Sun","Mon","Tues","Wed","Thurs","Frid","Sat"];
+var dayofweek= days[d.getDay()];
+var year = d.getFullYear();
+var listmonths = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
+var month = listmonths[d.getMonth()]; 
+var date = d.getDate();
+var xdate = dayofweek + " " + month + " " + date + " " + year;
  
+employees[i].hiredate = xdate;
 employees[i].employeeId = Math.floor(Math.random()*90000000) + 10000000;
 
   
 var added = "Employee Added";
 var txt = added.fontsize(5);
 var guy = i + 1;
-var foostring = txt.bold(5) + "<br/><br/>"+ "Name: " + employees[i].lname + ", "+ employees[i].fname +  "<br />" + "Department: " + employees[i].department + "<br/>" + "Employee ID: " + employees[i].employeeId + "<br/>" + "Total Employees: " + guy;
+var foostring = txt.bold(5) + "<br/><br/>"+ "Name: " + employees[i].lname + ", "+ employees[i].fname +  "<br />" + "Department: " + employees[i].department + "<br/>" + "Hire Date: " + xdate + "<br/>" + "Employee ID: " + employees[i].employeeId + "<br/>" + "Total Employees: " + guy;
 
 foostring.toString();
 document.getElementById("foo").innerHTML = foostring;
@@ -27,6 +38,8 @@ i++;
 return false;
   
 }
+
+  
 
 
 function init() {
